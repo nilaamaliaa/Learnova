bfd6a6e60d06b6294f3d443e8a0208c1cdafd608
galleanmvd <124250039@student.upnyk.ac.id>
2026-05-16 00:06:07 +0700

    memperbaiki proses register

--- proses_register.php
+++ proses_register.php
@@ -4,7 +4,13 @@ include 'koneksi.php';
 $username = $_POST['username'];
 $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-mysqli_query($conn, "INSERT INTO users (username, password) VALUES ('$username', '$password')");
+// Cek username sudah ada belum
+$cek = mysqli_query($conn, "SELECT * FROM users WHERE username='$username'");
+if (mysqli_num_rows($cek) > 0) {
+    echo "<script>alert('Username sudah dipakai!'); window.location='register.php';</script>";
+    exit();
+}
 
+mysqli_query($conn, "INSERT INTO users (username, password) VALUES ('$username', '$password')");
 header("Location: login.php");
 ?>
